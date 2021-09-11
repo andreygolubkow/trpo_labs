9f2a32bab54c99f385627c8b1a09f138611c43ac
AndreyGolubkow <AndreyGolubkow@gmail.com>
2021-09-11 22:17:26 +0700

    В ConsoleTest сделан ввод адреса доставки

--- PowerfulDiscounts.App.ConsoleTest/Program.cs
+++ PowerfulDiscounts.App.ConsoleTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using PowerfulDiscounts.Model.Common;
+using PowerfulDiscounts.Model.Models;
 
 namespace PowerfulDiscounts.App.ConsoleTest
 {
@@ -6,7 +8,20 @@ namespace PowerfulDiscounts.App.ConsoleTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите адрес доставки:");
+            Order order = null;
+            do
+            {
+                var address = Console.ReadLine();
+                var message = Act.Try(() =>
+                {
+                    order = Order.Create(address);
+                });
+                if (message != null)
+                {
+                    Console.WriteLine(message);
+                }
+            } while (order == null);
         }
     }
 }
