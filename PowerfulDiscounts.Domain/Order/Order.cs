fae5a970a802730a23b8a4794df2244c228594da
AndreyGolubkow <AndreyGolubkow@gmail.com>
2021-12-19 13:20:48 +0700

    Добавлен объект Заказ

---
+++ PowerfulDiscounts.Domain/Order/Order.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using PowerfulDiscounts.Model.Annotations;
+
+namespace PowerfulDiscounts.Model.Order
+{
+    /// <summary>
+    /// Заказ.
+    /// </summary>
+    public class Order: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private List<OrderItem> _items = new List<OrderItem>();
+
+        /// <summary>
+        /// Создать заказ.
+        /// </summary>
+        /// <param name="user"></param>
+        public Order(string user)
+        {
+            User = user;
+        }
+
+        /// <summary>
+        /// Автор заказа.
+        /// </summary>
+        public string User { get; }
+
+        /// <summary>
+        /// Предметы в заказе.
+        /// </summary>
+        public IReadOnlyCollection<OrderItem> Items => _items;
+
+        /// <summary>
+        /// Добавить элемент в заказ.
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddItem(OrderItem item)
+        {
+            if (Items.Any(i => i.Item.Equals(item.Item))) throw new ArgumentException("Такой объект уже есть в заказе");
+
+            _items.Add(item);
+            item.PropertyChanged += Item_PropertyChanged;
+
+
+            OnPropertyChanged(nameof(Items));
+        }
+
+        /// <summary>
+        /// Удалить элемент из заказа.
+        /// </summary>
+        /// <param name="item"></param>
+        public void RemoveItem(OrderItem item)
+        {
+            if (Items.All(i => i != item)) throw new ArgumentException("Такого товара нет в заказе");
+
+            item.PropertyChanged += Item_PropertyChanged;
+            _items.Remove(item);
+
+            OnPropertyChanged(nameof(Items));
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(Items));
+        }
+    }
+}
---
+++ PowerfulDiscounts.Domain/Order/OrderItem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PowerfulDiscounts.Model.Annotations;
+
+namespace PowerfulDiscounts.Model.Order
+{
+    /// <summary>
+    /// Предмет в заказе
+    /// </summary>
+    public class OrderItem: INotifyPropertyChanged
+    {
+        private double _count;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="item">Предмет</param>
+        /// <param name="price">Стоимость 1 шт.</param>
+        /// <param name="count">Кол-во</param>
+        public OrderItem(string item, double price, double count)
+        {
+            Item = item;
+            Price = price;
+            _count = count;
+        }
+
+        /// <summary>
+        /// Предмет заказа.
+        /// </summary>
+        public string Item { get; }
+
+        /// <summary>
+        /// Стоимость 1 единицы.
+        /// </summary>
+        public double Price { get; }
+
+        /// <summary>
+        /// Количество.
+        /// </summary>
+        public double Count
+        {
+            get { return _count; }
+            private set
+            {
+                if (value <= 0) throw new ArgumentException("Неверное кол-во товара");
+
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Изменить количество товара.
+        /// </summary>
+        /// <param name="count"></param>
+        public void ChangeCount(double count)
+        {
+            Count = count;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
